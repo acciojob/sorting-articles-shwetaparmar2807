ffd41c07cad51e2ab7798d4f22cbc13a02f78a0b
shwetaparmar2807 <shwetaparmar2807@gmail.com>
2025-02-21 16:08:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 //your JS code here. If required.
 let arr = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+let obj={};
 for(let i=0;i<arr.length;i++){
 	let newArr = arr[i].split(" ")
 	console.log(newArr);
-	if(newArr[0]=="a"||newArr[0]=="an"||newArr[0]=="the"){
+	if(newArr[0]=="A"||newArr[0]=="An"||newArr[0]=="the"){
 		newArr.splice(0,1);
 	}
 	console.log(newArr)
